1e1b89976cf4b1d2dea6febc4f5efeb7f7ba3041
Aditya <adityapratamax11@gmail.com>
2020-10-23 23:15:58 +0700

    tambah pagination ke view roles

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -9,7 +9,7 @@ class RolesController extends Controller
 {
     //
     public function index() {
-        $roles = Role::all();
+        $roles = Role::paginate(1);
         return view('roles',['roles' => $roles]);
     }
 }
